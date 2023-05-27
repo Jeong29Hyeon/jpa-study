60f2bb83643e65355aa6a266318888337c3dfe5b
jeong29hyeon <sian0369@naver.com>
2023-05-28 01:03:04 +0900

    #26 - 지연로딩과 즉시로딩
    실무에선 무조껀 지연로딩으로 써야 쿼리가 무분별하게 나가는걸 방지할 수 있다.

--- jpa-study/src/main/java/com/hellojpa/Member.java
+++ jpa-study/src/main/java/com/hellojpa/Member.java
@@ -3,6 +3,7 @@ package com.hellojpa;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -17,7 +18,7 @@ public class Member extends BaseEntity{
     @Column(name="USERNAME")
     private String userName;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="TEAM_ID")
     private Team team;
 
